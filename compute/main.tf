49e1c59db321f43db941c5a01345ed6a6e937de0
Arthofer <56601366+Arthofer@users.noreply.github.com>
2020-06-06 16:41:43 -0700

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -18,5 +18,4 @@ resource "aws_instance" "ubuntu" {
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
   availability_zone = "${var.aws_region}a"
-  key_name = "mattpeters"
 }
