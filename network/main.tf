06dbff3aadfd882fee9e9ea3217ca3e55629915b
Arthofer <56601366+Arthofer@users.noreply.github.com>
2020-06-06 05:06:56 -0700

    Update main.tf

--- network/main.tf
+++ network/main.tf
@@ -13,15 +13,8 @@ provider "vault" {
   max_lease_ttl_seconds = 1500
 }
 
-# AWS credentials from Vault
-# Must set up AWS backend in Vault on path aws with role deploy
-data "vault_aws_access_credentials" "aws_creds" {
-  backend = "aws-tf"
-  role = "deploy"
-}
-
 provider "aws" {
   region = "us-east-1"
-  access_key = "${data.vault_aws_access_credentials.aws_creds.access_key}"
-  secret_key = "${data.vault_aws_access_credentials.aws_creds.secret_key}"
+  access_key = "AKIAIWRKGG45JW33UDHQ"
+  secret_key = "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"
 }
